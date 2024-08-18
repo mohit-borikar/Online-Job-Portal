77754cf8fc8fb3ec4bf72096d29fc0697086963b
mohit-borikar <mohit2borikar@gmail.com>
2024-08-19 00:12:54 +0530

    Adding Header and Login Button

--- src/Layouts/app-layout.jsx
+++ src/Layouts/app-layout.jsx
@@ -1,11 +1,18 @@
+import Header from '@/components/header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
     <div>
-        <div className='grid-background'></div>
-        <Outlet/> 
+      <div className='grid-background'></div>
+      <main className='min-h-screen container'>
+        <Header/>
+        <Outlet />
+      </main>
+      <div className='p-10 text-center bg-gray-800 mt-10'>
+        Made By Mohit Borikar
+      </div>
     </div>
   )
 }

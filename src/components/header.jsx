ec83344f134a45c2f6f8a29388b2c17290331be1
mohit-borikar <mohit2borikar@gmail.com>
2024-08-27 11:51:10 +0530

    Login and Database
    packages were imported and link to database and for login as well

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 function Header() {
   return (
     <>
-    <nav className='py-4 flex justify-between items-center '>
-      <Link>
-      <img src="/logo.png" className='h-20' />
-      </Link>
+      <nav className='py-4 flex justify-between items-center '>
+        <Link>
+          <img src="/logo.png" className='h-20' />
+        </Link>
 
-      <Button variant='outline'>Login</Button>
-    </nav>
+        <Button variant='outline'>Login</Button>
+
+        {/* <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn> */}
+        
+      </nav>
     </>
   )
 }
